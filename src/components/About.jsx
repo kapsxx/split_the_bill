0af5ec141756b4c14ecd4e232738c71bfee559f9
Kapil Pant <kapil110198@gmail.com>
2021-11-04 00:53:02 +0530

    some minor changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import {motion} from "framer-motion";
 import { useState } from "react";
+import styles from "./About.module.css";
 
 function About(){
 
@@ -7,16 +8,22 @@ function About(){
 
     return (
         <>
-        <h2>About Page</h2>
-        <button onClick={()=>setTest(!test)}>test</button>
+        {/* <h2>About Page</h2> */}
+        {/* <button onClick={()=>setTest(!test)}>test</button> */}
 
-        {
+        {/* {
             test && <motion.div animate={{height: "100%"}}  style={{border: "2px solid red", width: "45%", margin: "auto", height: "0px", overflow: "hidden"}}>
                          hello
                     </motion.div>
-        }
+        } */}
 
-
+        <div className={styles.container}>
+            <div className={styles.sub_cont}>
+                <li>Once you click on "Back", make sure you refresh the application</li>
+                <li>Do not enter a very big number in place of "Amount Paid" as it may exceed the limit</li>
+                <li>Try to keep the number of members of a group to be minimal as it may break the application</li>
+            </div>
+        </div>
         </>
     )
 }
