92f6a9a09a763d1a2dbc8d280103e2eb2c6eaa0a
Kapil Pant <kapil110198@gmail.com>
2021-12-16 15:05:33 +0530

    paytm link added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from "./components/Navbar.jsx";
 import Routes from "./routes/Routes.jsx";
+import paytm from "./images/paytm.svg";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Routes/>
+      <div className='paytm_container'>
+        <a className='paytm_link' href='https://paytm.com/' target={"_blank"}>
+          <img src={paytm} alt='paytm' />
+        </a>
+      </div>
     </div>
   );
 }
